7fba4442d953b2b1d5926798865093799e65db89
G-Baby <gpw10097@saintjoe.edu>
2015-03-30 12:53:10 -0500

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,8 +1,8 @@
 var stock = "1 lemon, 2 cabbages, and 101 eggs";
 function minusOne(match, amount, unit) {
   amount = Number(amount) - 1;
-  if (amount == 1) // only one left, remove the 's'
-    unit = unit.slice(0, unit.length - 1);
+  if (amount == >100) // greater than 100
+    unit = "Zillions of";
   else if (amount == 0)
     amount = "no";
   return amount + " " + unit;
